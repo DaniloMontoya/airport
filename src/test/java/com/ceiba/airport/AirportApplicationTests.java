9612a2c392784240b060d7aeb21200eeeee28aec
Luis Miguel Montes Zuluaga - Ceiba Software <luis.montes@ceiba.com.co>
2020-12-17 11:10:43 -0500

    First commit

---
+++ src/test/java/com/ceiba/airport/AirportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceiba.airport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
