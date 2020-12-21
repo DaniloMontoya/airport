4553313120effa6a7622da3e6e96145288ecbb1e
Luis Miguel Montes Zuluaga - Ceiba Software <luis.montes@ceiba.com.co>
2020-12-21 16:59:29 -0500

    Se añade test para crear flight

--- src/test/java/co/com/ceiba/airport/domain/services/CreateFlightServiceTest.java
+++ src/test/java/co/com/ceiba/airport/domain/services/CreateFlightServiceTest.java
@@ -6,12 +6,32 @@ import co.com.ceiba.airport.domain.exceptions.InvalidTimeException;
 import co.com.ceiba.airport.domain.models.entities.Flight;
 import co.com.ceiba.airport.domain.ports.repositories.FlightRepository;
 import co.com.ceiba.airport.domain.testdatabuilder.FlightTestDataBuilder;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class CreateFlightServiceTest {
+
+    @Test
+    public void createFlightTest(){
+        //arrange
+        FlightTestDataBuilder flightTestDataBuilder = new FlightTestDataBuilder();
+        flightTestDataBuilder.conId("London-123456789");
+        Flight flight = flightTestDataBuilder.build();
+        FlightRepository flightRepository = mock(FlightRepository.class);
+        when(flightRepository.isExiste(flight.getId())).thenReturn(false);
+        when(flightRepository.isValidateTime(flight.getTime())).thenReturn(true);
+        when(flightRepository.createFlight(flight)).thenReturn(flight.getId());
+        CreateFlightService createFlightService = new CreateFlightService(flightRepository);
+
+        //act
+        String id = createFlightService.run(flight);
+
+        //asset
+        Assert.assertEquals(id, flight.getId());
+    }
 
     @Test
     public void validatePreviousFlightExistTest() {
@@ -30,7 +50,6 @@ public class CreateFlightServiceTest {
     public void validateTimeCalendar5MinTest(){
         //arrange
         FlightTestDataBuilder flightTestDataBuilder = new FlightTestDataBuilder();
-        flightTestDataBuilder.conId("London-12345");
         Flight flight = flightTestDataBuilder.build();
         FlightRepository flightRepository = mock(FlightRepository.class);
         when(flightRepository.isExiste(flight.getId())).thenReturn(false);
@@ -39,7 +58,10 @@ public class CreateFlightServiceTest {
         CreateFlightService createFlightService = new CreateFlightService(flightRepository);
 
         //act - assert
-        BaseTest.assertThrows(() -> createFlightService.run(flight), InvalidTimeException.class, "The flight time is invalid in the calendar"); }
+        BaseTest.assertThrows(() -> createFlightService.run(flight), InvalidTimeException.class, "The flight time is invalid in the calendar");
+    }
+
+
 }
 
 
