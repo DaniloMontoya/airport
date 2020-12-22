0f5f1adbd28cc589ee77816bc8e4864a3d5b8970
Luis Miguel Montes Zuluaga - Ceiba Software <luis.montes@ceiba.com.co>
2020-12-22 09:29:36 -0500

    Se eliminan algunas clases inecesarias

--- src/main/java/co/com/ceiba/airport/domain/ArgumentsValidater.java
+++ src/main/java/co/com/ceiba/airport/domain/ArgumentsValidater.java
@@ -1,9 +1,6 @@
 package co.com.ceiba.airport.domain;
 
-import co.com.ceiba.airport.domain.exceptions.InvalidValueException;
 import co.com.ceiba.airport.domain.exceptions.MandatoryValueException;
-
-import java.util.List;
 
 public class ArgumentsValidater {
 
