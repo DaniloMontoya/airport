96e8967382d51515bc7306e6a10c527bab776b99
Luis Miguel Montes Zuluaga - Ceiba Software <luis.montes@ceiba.com.co>
2020-12-23 15:38:49 -0500

    Refactor

--- src/main/java/co/com/ceiba/airport/domain/models/entities/Flight.java
+++ src/main/java/co/com/ceiba/airport/domain/models/entities/Flight.java
@@ -4,12 +4,12 @@ import static co.com.ceiba.airport.domain.ArgumentsValidater.mandatoryValidate;
 
 import lombok.Getter;
 
-
-
 @Getter
 public class Flight {
 
-    private static final String IT_IS_NECESSARY_TO_ENTER_THE_TIME = "it is necessary to enter the time";
+    private static final String IT_IS_NECESSARY_TO_ENTER_THE_TIME = "It is necessary to enter the time";
+    private static final String IT_IS_NECESSARY_TO_ENTER_THE_ARRIVAL = "It is necessary to enter the arrival";
+    private static final String MINIMUM_LENGTH = "El costo debe tener un valor mayor a cero";
 
     private String id;
     private long timeDeparture;
