eaf85c7fb41541610b3f6e1fb4e7e76037386471
Luis Miguel Montes Zuluaga - Ceiba Software <luis.montes@ceiba.com.co>
2020-12-24 15:57:54 -0500

    Se añade exception handler

---
+++ src/main/java/co/com/ceiba/airport/infrastructure/error/Error.java
@@ -0,0 +1,20 @@
+package co.com.ceiba.airport.infrastructure.error;
+
+public class Error {
+
+    private String nameException;
+    private String message;
+
+    public Error(String nameException, String message) {
+        this.nameException = nameException;
+        this.message = message;
+    }
+
+    public String getNameException() {
+        return nameException;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
