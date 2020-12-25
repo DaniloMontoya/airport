9e12c6a6319bfd441558fc2c4591700696119997
Luis Miguel Montes Zuluaga - Ceiba Software <luis.montes@ceiba.com.co>
2020-12-25 13:21:55 -0500

    se elimina interfaz dao

--- src/main/java/co/com/ceiba/airport/domain/models/dao/FlightDAO.java
+++
@@ -1,9 +0,0 @@
-package co.com.ceiba.airport.domain.models.dao;
-
-import java.util.List;
-import co.com.ceiba.airport.domain.models.dto.FlightDTO;
-
-public interface FlightDAO {
-    List<FlightDTO> getAllFlight();
-    FlightDTO getFlightBydId();
-}
