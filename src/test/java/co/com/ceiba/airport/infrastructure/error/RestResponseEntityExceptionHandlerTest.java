4970629fb8f780dfa95624b8304a6141f2d21ab5
Luis Miguel Montes Zuluaga - Ceiba Software <luis.montes@ceiba.com.co>
2020-12-29 10:42:10 -0500

    Se añade test para error por el lado del cliente

--- src/test/java/co/com/ceiba/airport/infrastructure/error/RestResponseEntityExceptionHandlerTest.java
+++ src/test/java/co/com/ceiba/airport/infrastructure/error/RestResponseEntityExceptionHandlerTest.java
@@ -1,13 +1,44 @@
 package co.com.ceiba.airport.infrastructure.error;
 
+import co.com.ceiba.airport.domain.models.entities.Flight;
+import co.com.ceiba.airport.domain.ports.repositories.FlightRepository;
+import co.com.ceiba.airport.domain.services.CreateFlightService;
+import co.com.ceiba.airport.domain.validators.ValidateCalendar;
+import co.com.ceiba.airport.domain.validators.ValidateExistence;
+import co.com.ceiba.airport.testdatabuilder.FlightTestDataBuilder;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.ResponseEntity;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
 class RestResponseEntityExceptionHandlerTest {
+
+    @Test
+    void handleConflictWithConflicServerTest(){
+        //arrange
+        try {
+            FlightTestDataBuilder flightTestDataBuilder = new FlightTestDataBuilder();
+            flightTestDataBuilder.conId("London-123456789");
+            Flight flight = flightTestDataBuilder.build();
+            FlightRepository flightRepository = mock(FlightRepository.class);
+            ValidateExistence validateExistence = mock(ValidateExistence.class);
+            ValidateCalendar validateCalendar = mock(ValidateCalendar.class);
+            when(validateExistence.isExist(flight.getId())).thenReturn(true);
+            CreateFlightService createFlightService = new CreateFlightService(flightRepository, validateExistence, validateCalendar);
+            createFlightService.run(flight);
+            Assert.fail();
+        }catch (Exception e){
+            //act
+            RestResponseEntityExceptionHandler restResponseEntityExceptionHandler = new RestResponseEntityExceptionHandler();
+            ResponseEntity<Error> resultado = restResponseEntityExceptionHandler.handleConflict(e);
+            //assert
+            Assert.assertEquals("El vuelo ya existe", resultado.getBody().getMensaje());
+        }
+
+    }
 
     @Test
     void handleConflictWithConflictServerTest(){
