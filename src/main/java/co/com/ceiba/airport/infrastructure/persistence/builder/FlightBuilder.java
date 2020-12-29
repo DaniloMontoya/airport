306c38b0a86c1e6efd98904a352fafdc274790f0
Luis Miguel Montes Zuluaga - Ceiba Software <luis.montes@ceiba.com.co>
2020-12-29 09:50:32 -0500

    Se añade excepciones

--- src/main/java/co/com/ceiba/airport/infrastructure/persistence/builder/FlightBuilder.java
+++ src/main/java/co/com/ceiba/airport/infrastructure/persistence/builder/FlightBuilder.java
@@ -1,17 +1,19 @@
 package co.com.ceiba.airport.infrastructure.persistence.builder;
 
+import co.com.ceiba.airport.domain.exceptions.MandatoryValueException;
 import co.com.ceiba.airport.domain.models.dto.FlightDTO;
 import co.com.ceiba.airport.domain.models.entities.Flight;
 import co.com.ceiba.airport.infrastructure.persistence.entities.FlightEntity;
 
 public final class FlightBuilder {
 
+    private static final String FLIGHTENTITY_IS_NULL = "FlightEntity es null";
+    private static final String FLIGHT_IS_NULL = "Flight es null";
+
     private FlightBuilder(){}
 
     public static Flight convertToDomain(FlightEntity flightEntity){
-
-        Flight flight = null;
-
+        Flight flight;
         if(flightEntity != null){
             flight = new Flight(
                     flightEntity.getId(),
@@ -19,24 +21,29 @@ public final class FlightBuilder {
                     flightEntity.getArrival(),
                     flightEntity.getCost(),
                     flightEntity.isReprogrammed());
+        }else{
+            throw new MandatoryValueException(FLIGHTENTITY_IS_NULL);
         }
-
         return flight;
 
     }
 
     public static FlightEntity convertToEntity(Flight flight){
         FlightEntity flightEntity = new FlightEntity();
-        flightEntity.setId(flight.getId());
-        flightEntity.setTimeDeparture(flight.getTimeDeparture());
-        flightEntity.setArrival(flight.getArrival());
-        flightEntity.setCost(flight.getCost());
-        flightEntity.setReprogrammed(flight.isReprogrammed());
+        if(flight != null) {
+            flightEntity.setId(flight.getId());
+            flightEntity.setTimeDeparture(flight.getTimeDeparture());
+            flightEntity.setArrival(flight.getArrival());
+            flightEntity.setCost(flight.getCost());
+            flightEntity.setReprogrammed(flight.isReprogrammed());
+        }else{
+            throw new MandatoryValueException(FLIGHT_IS_NULL);
+        }
         return flightEntity;
     }
 
     public static FlightDTO convertToDTO(Flight flight){
-        FlightDTO flightDTO = null;
+        FlightDTO flightDTO;
         if(flight != null){
             flightDTO = new FlightDTO(
                     flight.getId(),
@@ -44,6 +51,8 @@ public final class FlightBuilder {
                     flight.getArrival(),
                     flight.getCost(),
                     flight.isReprogrammed());
+        }else{
+            throw new MandatoryValueException(FLIGHT_IS_NULL);
         }
         return flightDTO;
     }
